e44f3bb0548caadd40be68d49a9d27d1dbc16fc7
Marco-Sorrentino <94227887+marco-sorrentino@users.noreply.github.com>
2023-11-28 17:54:40 +0100

    Initial commit

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,21 @@
+import 'package:grocery_app/data/categories.dart';
+import 'package:grocery_app/models/category.dart';
+import 'package:grocery_app/models/grocery_item.dart';
+
+var groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
